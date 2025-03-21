aaaf138019f31c957b3f453c0f278420b2583191
Francisco Moreno <franciscoj.sparcoltary@gmail.com>
2025-03-21 01:42:40 -0600

    delete: environment.ts and change api keys

--- market/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  stripePrivateApiKey: 'sk_test_51HmRonJILryrAveDMWp6i4VK3WA8qx3rvUS70Hrx7qPXgt6F3E7dYNkg5NsYCl6VQN96K456Y8q54RtIe3VdUZCo00r1kYPfu8',
-  stripePublicApiKey: 'pk_test_51HmRonJILryrAveDP2tXBPC5Jz8GkA01LUUlGta4h3hyx3kLMgzvprimCn0Jjs48nXmTb0E6hbPZ2LtrAMa3ldcv00CVKLr6Gz',
-  appVersion: '0.0.0',
-  production: true,
-};
